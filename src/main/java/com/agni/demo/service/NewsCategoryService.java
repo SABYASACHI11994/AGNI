3c729c842fb1a05cf7bde9ac59e72352b267df27
Prateek Hemani (INDIA - HCIT) <PR356883@wipro.com>
2018-05-30 02:30:50 +0530

    service

---
+++ src/main/java/com/agni/demo/service/NewsCategoryService.java
@@ -0,0 +1,11 @@
+package com.agni.demo.service;
+
+import java.util.List;
+
+import com.agni.demo.data.NewsCategory;
+
+public interface NewsCategoryService
+{
+
+	List<NewsCategory> saveNewsCategory();
+}
---
+++ src/main/java/com/agni/demo/service/NewsCategoryServiceImpl.java
@@ -0,0 +1,17 @@
+package com.agni.demo.service;
+
+import java.util.List;
+
+import com.agni.demo.data.NewsCategory;
+
+public class NewsCategoryServiceImpl implements NewsCategoryService
+{
+
+	@Override
+	public List<NewsCategory> saveNewsCategory()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
