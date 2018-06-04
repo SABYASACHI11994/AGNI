31fa5cd1c73a293ee772079a6986961d4b3d7f46
Sabyasachi Sahoo (INDIA - HCIT) <SA356897@wipro.com>
2018-06-04 22:41:09 +0530

    news

--- src/main/java/com/agni/demo/controller/NewsController.java
+++ src/main/java/com/agni/demo/controller/NewsController.java
@@ -4,17 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.agni.demo.data.News;
-import com.agni.demo.data.Person;
 import com.agni.demo.service.NewsService;
 import com.agni.demo.service.PersonService;
+import com.agni.demo.util.OutputResponse;
 
 @RestController
 public class NewsController {
@@ -29,27 +30,48 @@ public class NewsController {
 	NewsService newsService;
 	
 	
-    @RequestMapping(value = "/findByLastName",method = { RequestMethod.GET })
-    public List<Person> findByLastName(@RequestParam(value="name", defaultValue="World") String name) {
-        return personService.findByLastName(name);
-    }
-    
-    @RequestMapping(value = "/createPerson",method = { RequestMethod.POST })
-    public List<Person> createPerson(@RequestBody List<Person> name) {
-    	System.out.println(name);
-        return personService.savePerson(name);
-    }
+//    @RequestMapping(value = "/findByLastName",method = { RequestMethod.GET })
+//    public List<Person> findByLastName(@RequestParam(value="name", defaultValue="World") String name) {
+//        return personService.findByLastName(name);
+//    }
+//    
+//    @RequestMapping(value = "/createPerson",method = { RequestMethod.POST })
+//    public List<Person> createPerson(@RequestBody List<Person> name) {
+//    	System.out.println(name);
+//        return personService.savePerson(name);
+//    }
     
     @RequestMapping(value = "/createNews",method = { RequestMethod.POST })
-    public List<News> createNews(@RequestBody List<News> name) {
+    public String createNews(@RequestBody List<News> name) {
     	System.out.println(name);
-        return newsService.saveNews(name);
+    	OutputResponse response=new OutputResponse();
+		try {
+			List<News> news=newsService.saveNews(name);
+			response.setResponse(news.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response.setError(e);
+		}
+		return response.toString();
+//        return ;
     }
     
-    @RequestMapping(value = "/getLatestNews",method = { RequestMethod.POST })
-    public Page<News> getLatestNews(@RequestParam String name,Pageable pageable) {
-    	System.out.println(name);
-    	
-        return newsService.getNews(name,pageable);
+    @RequestMapping(value = "/getLatestNews/{name}/{page}/{size}",method = { RequestMethod.GET })
+    public Page<News> getLatestNews(@PathVariable("name") String name,@PathVariable("page") Integer page,@PathVariable("size")Integer size) {
+//    	System.out.println(name);
+//    	OutputResponse response=new OutputResponse();
+//    	Page<News> news=new 
+//		try {
+    	Pageable pageable=new PageRequest(page, size);
+			return newsService.getNews(name,pageable);
+////			response.setResponse(news.toString());
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			response.setError(e);
+//		}
+//		return news;
+//        return ;
     }
 }
--- src/main/java/com/agni/demo/data/News.java
+++ src/main/java/com/agni/demo/data/News.java
@@ -4,6 +4,10 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+
+import com.agni.demo.util.OutputMapper;
+import com.google.gson.annotations.Expose;
 
 import lombok.Data;
 
@@ -12,37 +16,48 @@ import lombok.Data;
 public class News
 {
 	@Id
+	@Expose
 	private String id;
-	
+	@Expose
 	private String newsHeading;
-	
+	@Expose
 	private String newsSubHeading;
-	
+	@Expose
 	private String newsContent;
-	
+	@Expose
 	private String newsSubContent;
-	
+//	@Expose
 //	@Embedded
 //	private NewsCategory newsCategory;
+	@Expose
 	private String author;
-	
+	@Expose
 	private Date validFrom;
-	
+	@Expose
 	private Date validTill;
-	
+	@Expose
 	private List<Image> images;
-	
+	@Expose
 	private List<String> links;
-	
+	@Expose
 	private List<Document> documents;
-	
+	@Expose
 	private List<NewsCategory> newsCategory;
-	
+	@Expose
 	private String createdBy;
-	
+	@Expose
 	private Date createdDate=new Date();
-	
+	@Expose
 	private String modifiedBy;
-	
+	@Expose
 	private Date modifiedDate;
+	
+//	@Transient
+//	private OutputMapper outputMapper = new OutputMapper();
+//
+//	@Override
+//	public String toString() {
+//		return outputMapper.gson().toJson(this);
+//	}
+	
 }
